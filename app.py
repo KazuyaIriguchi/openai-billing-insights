9fd60e91bca06f90be383cb787ed2892e8c14fa2
Kazuya Iriguchi <kazuya.iriguchi.j6k@jp.denso.com>
2023-08-04 14:01:53 +0900

    add: ability to upload and visualize files

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+import json
 
 from get_api_usage import get_monthly_usage
 
@@ -23,29 +24,61 @@ def update_progress(progress: float):
     progress_bar.progress(progress)
     progress_text.text(f"Processing {int(progress * 100)}% completed.")
 
+def load_monthly_usage(uploaded_file) -> dict:
+    """アップロードされたひと月の使用状況JSONファイルを読み込み
+
+    Args:
+        uploaded_file (): アップロードされたJSONファイル
+
+    Returns:
+        dict: 使用状況データ
+    """
+    # Read the uploaded file as bytes
+    file_bytes = uploaded_file.read()
+
+    # Convert bytes to a string
+    file_str = file_bytes.decode("utf-8")
+
+    # Load the JSON content from the string
+    json_data = json.loads(file_str)
+
+    return json_data
+
 st.title("OpenAI API Monthly Usage")
+# Let the user choose between API and File
+option = st.radio("Choose data source:", ("Use API", "Upload File"))
+use_api = True
 
-# get the current year and month
-current_year, current_month = get_current_year_month()
-# Define available years and months
-available_years = list(range(2020, 2024))
-available_months = list(range(1, 13))
+if option == "Use API":
+    # get the current year and month
+    current_year, current_month = get_current_year_month()
+    # Define available years and months
+    available_years = list(range(2020, 2024))
+    available_months = list(range(1, 13))
 
-# Find the index of the current year and month
-default_year_index = available_years.index(current_year)
-default_month_index = available_months.index(current_month)
+    # Find the index of the current year and month
+    default_year_index = available_years.index(current_year)
+    default_month_index = available_months.index(current_month)
 
-# Create select boxes for year and month with default values
-selected_year = st.selectbox("Select Year:", available_years, index=default_year_index)
-selected_month = st.selectbox("Select Month:", available_months, index=default_month_index)
+    # Create select boxes for year and month with default values
+    selected_year = st.selectbox("Select Year:", available_years, index=default_year_index)
+    selected_month = st.selectbox("Select Month:", available_months, index=default_month_index)
+elif option == "Upload File":
+    # Let the user upload a file
+    uploaded_file = st.file_uploader("Choose a file:", type=["json", "csv"])
+    use_api = False
 
 progress_bar = st.progress(0)
 progress_text = st.empty()
 
 if st.button("Get Usage"):
-    year_month = f"{selected_year}-{selected_month:02}"
     # 各モデルの合計コストを計算
-    monthly_usage = get_monthly_usage(year_month, callback=update_progress)
+    monthly_usage = {}
+    if use_api:
+        year_month = f"{selected_year}-{selected_month:02}"
+        monthly_usage = get_monthly_usage(year_month, callback=update_progress, wait_time=0.5, dryrun=True)
+    else:
+        monthly_usage = load_monthly_usage(uploaded_file)
 
     progress_bar.progress(100)
     progress_text.text("Completed!")
